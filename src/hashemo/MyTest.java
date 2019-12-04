672858cd3181fc2af5f8cacb32269db3cca49d2c
sunnydaydev <1246771571@qq.com>
2019-12-04 13:24:17 +0800

    hash table study

---
+++ src/hashemo/MyTest.java
@@ -0,0 +1,13 @@
+package hashemo;
+
+import java.util.Hashtable;
+
+/**
+ * Created by sunnyDay on 2019/12/4 13:14
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        Hashtable<String,String>hashtable = new Hashtable<>();
+
+    }
+}
